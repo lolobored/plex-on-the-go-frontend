e8485942c34089d6a4d7c6a0e43302b9fa47688d
Laurent Laborde <llaborde@bottomline.com>
2018-06-30 17:22:56 +0100

    Updated password policy

--- src/app/components/settings/users-settings/users-edit/custom.validator.ts
+++ src/app/components/settings/users-settings/users-edit/custom.validator.ts
@@ -71,6 +71,6 @@ export const errorMessages: { [key: string]: string } = {
   fullName: 'Full name must be between 1 and 128 characters',
   email: 'Email must be a valid email address (username@domain)',
   confirmEmail: 'Email addresses must match',
-  password: 'Password must be between 7 and 100 characters, and contain at least one number and special character',
+  password: 'Password must be between 7 and 100 characters',
   confirmPassword: 'Passwords must match'
 };
